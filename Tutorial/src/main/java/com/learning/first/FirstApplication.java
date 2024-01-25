ddec7b3b87aa5cd3ae90e90aad717ceb9acf9034
Remian 9080 <rakibpsp@gmail.com>
2024-01-26 01:40:46 +0600

    New Solved

--- Tutorial/src/main/java/com/learning/first/FirstApplication.java
+++ Tutorial/src/main/java/com/learning/first/FirstApplication.java
@@ -1,9 +1,14 @@
 package com.learning.first;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.first.student.Student;
 
 @SpringBootApplication
 @RestController 
@@ -14,9 +19,15 @@ public class FirstApplication {
 	 }
 	 
 	 @GetMapping 
-	 public String hello()
+	 public List<Student> hello()
 	 {
-		return "Hello World";
+		
+		return List.of(
+			new Student(
+			1L,"Rakib","rakibpsp@gmail.com",
+		    LocalDate.of(1999,11 ,06 ),
+		    24
+		));
 	 }
  
 }
