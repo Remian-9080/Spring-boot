c56cc53361e53b00c4f77a7c44570ca721056ed8
Remian 9080 <rakibpsp@gmail.com>
2024-01-26 00:01:02 +0600

    New Solved

--- Tutorial/src/main/java/com/learning/first/FirstApplication.java
+++ Tutorial/src/main/java/com/learning/first/FirstApplication.java
@@ -2,12 +2,21 @@ package com.learning.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController 
 public class FirstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
-	}
-
+	 }
+	 
+	 @GetMapping 
+	 public String hello()
+	 {
+		return "Hello World";
+	 }
+ 
 }
